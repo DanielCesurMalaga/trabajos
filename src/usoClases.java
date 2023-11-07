8c9ee9f1571b23024092fc32d69b29a3d955e2e4
DanielDigytale <daniel.fernandez@digytale.net>
2023-11-07 11:22:39 +0100

    inicio

---
+++ src/usoClases.java
@@ -0,0 +1,19 @@
+
+public class usoClases {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Una una;
+		una = new Una(2);
+		System.out.println(una.x);
+		
+	}
+}
+
+class Una {
+	public int x;
+
+	public Una(int x) {
+		this.x = x;
+	}
+}
