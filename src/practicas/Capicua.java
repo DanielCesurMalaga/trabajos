02a48e0a27feaf06893bb56693f9a811d680736f
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2023-12-20 12:48:41 +0100

    version 1, facilita.

---
+++ src/practicas/Capicua.java
@@ -0,0 +1,25 @@
+package practicas;
+
+import java.util.Scanner;
+
+public class Capicua {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int num=0;
+
+		do {
+			do {
+				System.out.print("Introduce número de tres cifras: ");
+				num = teclado.nextInt();
+			} while ((num > 999) || (num < 100));
+			if (num / 100 == num % 10) {
+				System.out.println("el número es capicua");
+			}
+			System.out.println("SEGUIR? [si = 1]: ");			
+		} while (teclado.nextInt()==1);
+		
+		teclado.close();
+	}
+
+}
