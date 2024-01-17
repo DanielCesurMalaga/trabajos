ce662a6978584c05f48bac493c4786c7fb9e2381
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-01-17 13:33:32 +0100

    clase 17/01

--- src/practicas/SimuladorLista.java
+++ src/practicas/SimuladorLista.java
@@ -4,11 +4,11 @@ public class SimuladorLista {
 
 	public static void main(String[] args) {
 		MiArray yo = new MiArray(100);
-		for (int i=0; i<50;i++) {
-			yo.insertarValor(i+10);
+		for (int i = 0; i < 50; i++) {
+			yo.insertarValor(i + 10);
 			System.out.println(yo.getIntMiArray(i));
 		}
-		System.out.println("NumElems: "+yo.getNumElem());
+		System.out.println("NumElems: " + yo.getNumElem());
 	}
 
 } // cierra SimuladorLista
@@ -25,7 +25,7 @@ class MiArray {
 		numElem = 0;
 		miArray = new int[longitud];
 		for (int i = 0; i < longitud; i = i + 1) {
-			miArray[i] = 0;
+			miArray[i] = -1;
 		}
 
 	}
@@ -34,7 +34,7 @@ class MiArray {
 		numElem = 0;
 		miArray = new int[lONGITUD_DEFAULT];
 		for (int i = 0; i < miArray.length; i = i + 1) {
-			miArray[i] = 0;
+			miArray[i] = -1;
 		}
 	}
 
@@ -61,17 +61,47 @@ class MiArray {
 
 	public void resetear() {
 		for (int i = 0; i < miArray.length; i++) {
-			miArray[i] = 0;
+			miArray[i] = -1;
 		}
 	}
-	
+
 	public void insertarValor(int valor) {
 		if (numElem == miArray.length) {
 			System.out.println("Array lleno, no se ha insertado elemento");
 		} else {
-			miArray[numElem]=valor;
+			miArray[numElem] = valor;
 			numElem++;
 		}
 	}
-	
+
+	public int sacarValorUltimo() {
+		int temp;
+		if (numElem == 0) {
+			return -1;
+		} else {
+			numElem--;
+			temp=miArray[numElem];
+			miArray[numElem]=-1;
+			return temp;
+
+		}
+	}
+
+	public int sacarValorPrimero() {
+		int temp;
+		if (numElem == 0) {
+			return -1;
+		} else {
+			temp=miArray[0];
+			// reducir en 1 numElem y meter los valores en la posicion anterior.
+			for (int i=0; i<numElem-1;i++) {
+				miArray[i]=miArray[i+1];
+			}
+			miArray[numElem-1]=-1;
+			numElem--;
+			
+			return temp;
+		}
+	}
+
 } // cierra MiArray
