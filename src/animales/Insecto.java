a59872d9db222a71cfe9a41d66332965adee94c0
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-01-18 13:15:48 +0100

    clase 18/01 + tutoría prog

--- src/animales/Insecto.java
+++ src/animales/Insecto.java
@@ -3,36 +3,40 @@ package animales;
 public class Insecto extends Animal {
 	private boolean alas;
 	private boolean antenas;
-	private Enemigo[] enemigos;
+	private MiArrayEnemigos enemigos;
 	public String atributo;
-	
+
 	public Insecto(String nombre, int tpoVida, boolean alas, boolean antenas) {
 		super(nombre, tpoVida);
 		this.alas = alas;
 		this.antenas = antenas;
 		atributo = "soy insecto";
-		enemigos = new Enemigo[5];
+		enemigos = new MiArrayEnemigos();
 	}
+
 	public boolean isAlas() {
 		return alas;
 	}
+
 	public void setAlas(boolean alas) {
 		this.alas = alas;
 	}
+
 	public boolean isAntenas() {
 		return antenas;
 	}
+
 	public void setAntenas(boolean antenas) {
 		this.antenas = antenas;
 	}
-	
-	public Enemigo[] getEnemigos() {
+
+	public MiArrayEnemigos getEnemigos() {
 		return enemigos;
 	}
-	public void setEnemigo(String enemigo) {
-		enemigos[0]= new Enemigo(enemigo);
-	}
 
+	public void setEnemigo(String enemigo) {
+		enemigos.insertarValor(new Enemigo(enemigo)); 
+	}
 
 	class Enemigo {
 		private String nombre;
@@ -42,6 +46,10 @@ public class Insecto extends Animal {
 			this.nombre = nombre;
 		}
 
+		public Enemigo() {
+			this.nombre = null;
+		}
+
 		public String getNombre() {
 			return nombre;
 		}
@@ -49,8 +57,102 @@ public class Insecto extends Animal {
 		public void setNombre(String nombre) {
 			this.nombre = nombre;
 		}
-		
-	}
-	
-}
 
+	} // fin clase Enemigo
+
+	class MiArrayEnemigos {
+		// atributos
+		private Enemigo[] miArray;
+		private int numElem;
+		private static final int lONGITUD_DEFAULT = 10;
+		private int longitud;
+
+		// constructores
+		MiArrayEnemigos(int longitud) {
+			numElem = 0;
+			miArray = new Enemigo[longitud];
+			for (int i = 0; i < longitud; i = i + 1) {
+				miArray[i] = null;
+			}
+
+		}
+
+		MiArrayEnemigos() {
+			numElem = 0;
+			miArray = new Enemigo[lONGITUD_DEFAULT];
+			for (int i = 0; i < miArray.length; i = i + 1) {
+				miArray[i] = null;
+			}
+		}
+
+		// metodos
+		public int getNumElem() {
+			return numElem;
+		}
+
+		public void setNumElem(int numElem) {
+			this.numElem = numElem;
+		}
+
+		public Enemigo[] getMiArray() {
+			return miArray;
+		}
+
+		public Enemigo getEnemigoMiArray(int posicion) {
+			if ((posicion < miArray.length) && (posicion >= 0)) {
+				return miArray[posicion];
+			} else {
+				return null; // en caso de error al meter la posición
+			}
+		}
+
+		public void resetear() {
+			for (int i = 0; i < miArray.length; i++) {
+				miArray[i] = null;
+				numElem = 0;
+			}
+
+		}
+
+		public void insertarValor(Enemigo valor) {
+			if (numElem == miArray.length) {
+				System.out.println("Array lleno, no se ha insertado elemento");
+			} else {
+				miArray[numElem] = valor;
+				numElem++;
+			}
+		}
+
+		public Enemigo sacarValorUltimo() {
+			Enemigo temp = new Enemigo();
+			if (numElem == 0) {
+				return null;
+			} else {
+				numElem--;
+				temp = miArray[numElem];
+				miArray[numElem] = null;
+				return temp;
+
+			}
+		}
+
+		public Enemigo sacarValorPrimero() {
+			Enemigo temp = new Enemigo();
+			if (numElem == 0) {
+				return null;
+			} else {
+				temp = miArray[0];
+				// reducir en 1 numElem y meter los valores en la posicion anterior.
+				for (int i = 0; i < numElem - 1; i++) {
+					miArray[i] = miArray[i + 1];
+				}
+				miArray[numElem - 1] = null;
+				numElem--;
+
+				return temp;
+			}
+		}
+
+	} // cierra MiArray
+
+}
--- src/practicas/SimuladorLista.java
+++ src/practicas/SimuladorLista.java
@@ -1,107 +1,82 @@
 package practicas;
+import java.util.Random;
 
 public class SimuladorLista {
 
 	public static void main(String[] args) {
-		MiArray yo = new MiArray(100);
-		for (int i = 0; i < 50; i++) {
-			yo.insertarValor(i + 10);
-			System.out.println(yo.getIntMiArray(i));
+		
+		
+		MiArrayInt yo = new MiArrayInt(10);
+		Random aleatorio = new Random();
+	
+		// Rellena el array
+		for(int x = 0; x<10;x++) {
+			yo.insertarValor(aleatorio.nextInt(10));
+			System.out.print(yo.getIntMiArray(x)+" ");
 		}
-		System.out.println("NumElems: " + yo.getNumElem());
+		System.out.println();
+		
+		
+		MiArrayInt pares = new MiArrayInt(yo.numDePares());
+		MiArrayInt impares = new MiArrayInt(yo.getNumElem()-yo.numDePares());
+		
+		
+		
+		//separa par e impar
+		for(int x = 0; x<yo.getNumElem(); x++) {
+			if((yo.getIntMiArray(x)%2)==0) {
+				pares.insertarValor(yo.getIntMiArray(x));
+			}else {
+				impares.insertarValor(yo.getIntMiArray(x));
+			}
+		}
+		
+		
+		//muestra par e impar por pantalla
+		System.out.println("Array de pares:");
+		for(int x = 0; x<pares.getNumElem();x++) {
+			System.out.print(pares.getIntMiArray(x)+" ");
+		}
+		System.out.println();
+		System.out.println("Array de impares:");
+		for(int x = 0; x<impares.getNumElem();x++) {
+			System.out.print(impares.getIntMiArray(x)+" ");
+		}
+		
+		
+		
+		yo.resetear();
+		
+		//par e impar alternando
+		for(int x = 0; x<(pares.getNumElem()+impares.getNumElem()); x++) {
+			if (x<pares.getNumElem()) {
+				yo.insertarValor(pares.getIntMiArray(x));
+			}
+			if (x<impares.getNumElem()) {
+				yo.insertarValor(impares.getIntMiArray(x));
+			}
+		}
+		
+		//muestra el array ordenado
+		System.out.println();
+		System.out.println("Array ordenado:");
+		for(int x = 0; x<yo.getNumElem();x++) {
+			System.out.print(yo.getIntMiArray(x)+" ");
+		}
+		
+		
+		
 	}
 
 } // cierra SimuladorLista
 	// almacena enteros postivos
 
-class MiArray {
-	// atributos
-	private int[] miArray;
-	private int numElem;
-	private static final int lONGITUD_DEFAULT = 10;
 
-	// constructores
-	MiArray(int longitud) {
-		numElem = 0;
-		miArray = new int[longitud];
-		for (int i = 0; i < longitud; i = i + 1) {
-			miArray[i] = -1;
-		}
 
-	}
 
-	MiArray() {
-		numElem = 0;
-		miArray = new int[lONGITUD_DEFAULT];
-		for (int i = 0; i < miArray.length; i = i + 1) {
-			miArray[i] = -1;
-		}
-	}
 
-	// metodos
-	public int getNumElem() {
-		return numElem;
-	}
 
-	public void setNumElem(int numElem) {
-		this.numElem = numElem;
-	}
 
-	public int[] getMiArray() {
-		return miArray;
-	}
 
-	public int getIntMiArray(int posicion) {
-		if ((posicion < miArray.length) && (posicion >= 0)) {
-			return miArray[posicion];
-		} else {
-			return -1; // en caso de error al meter la posición
-		}
-	}
 
-	public void resetear() {
-		for (int i = 0; i < miArray.length; i++) {
-			miArray[i] = -1;
-		}
-	}
 
-	public void insertarValor(int valor) {
-		if (numElem == miArray.length) {
-			System.out.println("Array lleno, no se ha insertado elemento");
-		} else {
-			miArray[numElem] = valor;
-			numElem++;
-		}
-	}
-
-	public int sacarValorUltimo() {
-		int temp;
-		if (numElem == 0) {
-			return -1;
-		} else {
-			numElem--;
-			temp=miArray[numElem];
-			miArray[numElem]=-1;
-			return temp;
-
-		}
-	}
-
-	public int sacarValorPrimero() {
-		int temp;
-		if (numElem == 0) {
-			return -1;
-		} else {
-			temp=miArray[0];
-			// reducir en 1 numElem y meter los valores en la posicion anterior.
-			for (int i=0; i<numElem-1;i++) {
-				miArray[i]=miArray[i+1];
-			}
-			miArray[numElem-1]=-1;
-			numElem--;
-			
-			return temp;
-		}
-	}
-
-} // cierra MiArray
