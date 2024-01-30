a0dedc97cbb0d41507adc250959997b2b74a47ad
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-01-30 16:57:48 +0100

    formula uno de Adrián

--- src/animales/UsoAnimales.java
+++ src/animales/UsoAnimales.java
@@ -1,40 +1,15 @@
 package animales;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class UsoAnimales {
 
 	public static void main(String[] args) {
-		Mamifero perro = new Mamifero("perro",15, true);
-		Insecto hormiga = new Insecto("hormiga",1,false,true);
-		System.out.println("El animal: "+hormiga.getNombre()+ " tiene alas?: "+ hormiga.isAlas());
-		
-		Insecto atomica;
-		Insecto volantona;
-		volantona = hormiga;
-		atomica = new Insecto(
-				hormiga.getNombre()+" atomica",
-				hormiga.getTpoVida(),
-				hormiga.isAlas(),
-				hormiga.isAntenas()
-				);
-		hormiga.setAlas(true);
-		volantona.setNombre("volantona");
-		System.out.println(hormiga.getNombre()+" tiene alas?: "+ hormiga.isAlas());
-		System.out.println(atomica.getNombre()+" tiene alas?: "+ atomica.isAlas());
-		System.out.println(volantona.getNombre()+" tiene alas?: "+ volantona.isAlas());
-		
-		System.out.println("Volantona dice: " + volantona.atributo);
-		volantona.atributo = "ya no soy insecto";
-		System.out.println("Volantona dice: " + volantona.atributo);
-		
-		Animal pruebaSonido = new Mamifero("Perro",14,true);
-		System.out.println("Clase de pruebaSonido: "+pruebaSonido.getClass());
-		System.out.println("Instancia de Mamifero?: "+ (pruebaSonido instanceof Mamifero));
-		System.out.println("Instancia de Animal?: "+ (pruebaSonido instanceof Animal));
-		System.out.println("Instancia de Object?: "+ (pruebaSonido instanceof Object));
-		pruebaSonido.sonido();
+		final Animal[] animales = new Animal[3];
+		animales[0]= new Animal("perro",10);
+		animales[1]= new Animal("gato",12);
+		animales[2]= new Animal("pajaro",5);
+		System.out.println(animales[0].getNombre());
+		animales[0]= new Animal("oso",15);
+		System.out.println(animales[0].getNombre());
 	}
 
 }
