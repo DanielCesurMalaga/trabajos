e30d67e77b3a5838f64719fec929c9780cd92b3c
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-03-14 08:53:45 +0100

    listasJava

---
+++ src/practicas/ListasJava.java
@@ -0,0 +1,40 @@
+package teoriaUD5;
+
+public class ListasJava {
+    public static void main(String[] args) {
+        ListaInt milista = new ListaInt("miLista");
+    }
+}
+
+class NodoListaInt {
+    // atributos
+    public int numero;
+    public NodoListaInt sig;
+
+    // constructor
+    public NodoListaInt(int num, NodoListaInt nodoSig) {
+        this.numero = num;
+        this.sig = nodoSig;
+
+    }
+}
+
+class ListaInt {
+    // atributos
+    public String nombre;
+    public NodoListaInt inicio;
+
+    // constructor
+    public ListaInt(String nombre) {
+        this.nombre = nombre;
+        this.inicio = null;
+    }
+
+    // métodos
+    public boolean insertar(int num) {
+
+        if (inicio == null) { // la lista está vacía
+
+        }
+    }
+}
